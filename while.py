5d2e6b2cabc0aed6a6f8eb62203c01379d0b4e3d
MohamadAmin <aminnoorinajafi@gmail.com>
2022-12-14 15:47:13 +0330

    Create while.py
    while loop in python

---
+++ while.py
@@ -0,0 +1,38 @@
+# i = 0 
+# while i <= 25:
+#     print(i)
+#     i += 2
+#     print("Progress run")
+
+
+
+# answer = int(input("player 1 : Please enter one number (1 - 100): "))
+
+# its_corect = False
+# try_count = 0
+
+# while try_count < 10 and its_corect == False:
+#     guess = int(input("player 2 : please enter one Guess : "))
+#     if guess == answer:
+#         print("player 2 : You win")
+#         its_corect == True
+#     elif guess > answer:
+#         print("anwser greater than is guess")
+#         try_count += 1
+#     else:
+#         print("answer smaller than is guess")
+
+
+# if its_corect == False:
+#     print("player 2 : you lost")
+    
+
+
+name = "Parsa"
+
+i = 0
+
+while i < 5:
+    print(name)
+    i += 1
+    print("ok")
